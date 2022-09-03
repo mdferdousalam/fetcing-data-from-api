ae84187e72a86826c4f1a2799992acc97da67e66
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-09-03 09:33:36 +0600

    Modal added

--- js/app.js
+++ js/app.js
@@ -101,12 +101,12 @@ const relativePost = async (category_id) => {
 
 
 
-                            <button onclick=showDetails(${
-                              post._id
-                            })> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                          </svg> </button>
-                          
+                             
+                          <label onclick=showDetails(${
+                            post._id
+                          }) for="my-modal-6" class="btn modal-button"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                        </svg></label>
 
 
                      
