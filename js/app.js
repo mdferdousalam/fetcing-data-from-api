65ee88a95bb8bde6a20aaf3f6c07afbbcfbd7f1a
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-09-03 08:08:48 +0600

    posts are added dynamically in th ui

--- js/app.js
+++ js/app.js
@@ -16,9 +16,9 @@ const displayDefaultMenu = async () => {
     // console.log(item);
     const menu = document.getElementById("menu");
     const menuDiv = document.createElement("a");
-    postId = item.category_id;
+    // postId = item.category_id;
     menuDiv.innerHTML = `
-        <a class="p-4 text-gray-500 hover:text-sky-400 active:text-sky-400" onclick="relativePost('${postId}')"> ${item.category_name} </a>
+        <a class="p-4 text-gray-500 hover:text-sky-400 active:text-sky-400" onclick="relativePost('${item.category_id}')"> ${item.category_name} </a>
     `;
     menu.appendChild(menuDiv);
   });
@@ -27,10 +27,66 @@ const displayDefaultMenu = async () => {
 // all time categories menu items should be displayed in the menu div   below the function call.
 displayDefaultMenu();
 
-const relativePost = async (postId) => {
-  const url = `https://openapi.programming-hero.com/api/news/category/${postId}`;
-  console.log(url);
+// menu click and more of that category news code here.
+const relativePost = async (category_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+  //   console.log(url);
   const post = await fetch(url);
   const details = await post.json();
-  console.log(details);
+  const detailsNewsDiv = document.getElementById("details-news");
+  detailsNewsDiv.innerHTML = "";
+  const categoryPost = details.data;
+  categoryPost.forEach((post) => {
+    // console.log(post);
+    const detailsDiv = document.createElement("div");
+    detailsDiv.innerHTML = `
+
+      <div class="ml-4 mt-7 flex">
+                <div class="flex-none">
+                <img class="img-responsive w-48 h-48" src="${
+                  post.thumbnail_url
+                }" alt="">
+              </div>
+                 <div class="grow mt-5 pl-4" >
+              <h1 class="text-xl font-bold">${post.title}</h1>
+              <p>${
+                post.details.length > 180
+                  ? post.details.slice(0, 180) + "..."
+                  : post.details
+              }</p>
+
+                  <div class="flex pt-3">
+                            <div class="flex-none pt-2"> <img class="img-responsive w-8 rounded-full" src="${
+                              post.author.img
+                            }" alt=""> 
+                            </div>
+
+                            <div <p class="grow pl-3 text-sm ">${
+                              post.author.name
+                                ? post.author.name
+                                : "author name not found"
+                            }</p> 
+                                <p class="text-sm">${post.author.published_date.slice(
+                                  0,
+                                  10
+                                )}</p> 
+                            </div>
+
+
+                            <div > <span id="font-awesome-icon">
+                            
+                            </span> <span> ${post.total_view} </span></div>
+                     
+                   </div>
+
+          </div>
+
+      </div>
+
+      `;
+
+    detailsNewsDiv.appendChild(detailsDiv);
+  });
+  console.log(details.data);
+  //   return details;
 };
