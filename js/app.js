82b91f2772804d81e98152f9ccae0f701715dcd5
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-09-03 13:38:32 +0600

    no news found pard added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+/*
+ক্যাটাগরি এ পি আই ফেচ ও লোড ফাংশন 
+
+*/
+
 const loadDefaultMenu = async () => {
   const url = `https://openapi.programming-hero.com/api/news/categories`;
   const res = await fetch(url);
@@ -7,11 +12,14 @@ const loadDefaultMenu = async () => {
 
 // loadDefaultMenu();
 
+/*
+ক্যাটাগরি গুলো মেনুতে সাজানোর ফাংশন 
+
+*/
 // menu items display function logic code here
 const displayDefaultMenu = async () => {
   const data = await loadDefaultMenu();
   const itemsArray = data.data.news_category;
-  // console.log(itemsArray);
   itemsArray.forEach((item) => {
     // console.log(item);
     const menu = document.getElementById("menu");
@@ -25,7 +33,16 @@ const displayDefaultMenu = async () => {
 };
 
 // all time categories menu items should be displayed in the menu div   below the function call.
+/*
+ক্যাটাগরি গুলো ডিফল্ট ভাবে লোড হয়ে থাকার জন্য কল করে রাখা ফাংশন। 
+
+*/
 displayDefaultMenu();
+
+/*
+যে কোন ক্যাটাগরিতে ক্লিক করলে ঐ সম্পর্কিত সকল নিউজ দেখানোর ফাংশন ও ইউ আই এখানে।
+
+*/
 
 // menu click and more of that category news code here.
 const relativePost = async (category_id) => {
@@ -33,11 +50,24 @@ const relativePost = async (category_id) => {
   //   console.log(url);
   const post = await fetch(url);
   const details = await post.json();
+
   const detailsNewsDiv = document.getElementById("details-news");
   detailsNewsDiv.innerHTML = "";
+
+  const notFound = document.getElementById("not-found");
+  notFound.textContent = "";
+
+  if (details.data.length === 0) {
+    // console.log("No news for that category");
+    notFound.innerText = "Sorry!!! No news found for This category";
+
+    return;
+  }
+
   const categoryPost = details.data;
   categoryPost.forEach((post) => {
-    // console.log(post);
+    console.log(post);
+
     const detailsDiv = document.createElement("div");
     detailsDiv.innerHTML = `
 
@@ -50,7 +80,7 @@ const relativePost = async (category_id) => {
                  <div class="grow mt-5 pl-4" >
               <h1 class="text-xl font-bold">${post.title}</h1>
               <p>${
-                post.details.length > 180
+                post.details.length >= 180
                   ? post.details.slice(0, 180) + "..."
                   : post.details
               }</p>
@@ -64,10 +94,11 @@ const relativePost = async (category_id) => {
                             <div <p class=" flex-none pl-3 text-sm ">${
                               post.author.name ? post.author.name : "not found"
                             }</p> 
-                                <p class="text-sm">${post.author.published_date.slice(
-                                  0,
-                                  10
-                                )}</p> 
+                                <p class="text-sm">${
+                                  post.author.published_date
+                                    ? post.author.published_date.slice(0, 10)
+                                    : "Not Found"
+                                }</p> 
                             </div>
 
 
@@ -99,13 +130,16 @@ const relativePost = async (category_id) => {
                              <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                            </svg>
                             </div>
-
-
-
-                             
-                          <label  for="my-modal-6" class="mx-auto  pt-2 btn modal-button"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                         
+                          <label for="my-modal-3" 
+                          onclick="showModal('${post.title}',
+                          '${post.image_url}','${
+      post.details
+    }')"  class="mx-auto btn btn-info modal-button"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                         </svg></label>
+
+                          
 
 
                      
@@ -123,6 +157,15 @@ const relativePost = async (category_id) => {
   return categoryPost;
 };
 
-const showDetails = () => {
-  console.log("clicked categoryPost");
+const showModal = (title, image, description) => {
+  // console.log(description, image)
+  const modalBody = document.getElementById("modal-body");
+  modalBody.textContent = "";
+  modalBody.innerHTML = `
+    <p class="py-4">
+    ${title}
+    </p>
+    <img src="${image}"/>
+    
+    <p class="py-4">${description}</p>`;
 };
