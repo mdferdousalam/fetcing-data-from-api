00ffb468f1fd7f6382da94e464f16c5b358242da
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-09-04 09:35:03 +0600

    Background color added

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ const displayDefaultMenu = async () => {
     const menuDiv = document.createElement("a");
     // postId = item.category_id;
     menuDiv.innerHTML = `
-        <a class="p-4 text-gray-500 hover:text-sky-400 active:text-sky-400" onclick="relativePost('${item.category_id}')"> ${item.category_name} </a>
+        <a class=" p-1 bg-sky-200  text-gray-500 hover:text-sky-400 active:text-sky-400" onclick="relativePost('${item.category_id}')"> ${item.category_name} </a>
         
     `;
 
@@ -88,7 +88,7 @@ const relativePost = async (category_id) => {
     const detailsDiv = document.createElement("div");
     detailsDiv.innerHTML = `
 
-      <div class="ml-4 mt-7 flex">
+      <div class="ml-4 my-7 md:flex sm:text-center md:text-left rounded bg-sky-200">
                 <div class="flex-none">
                 <img class="img-responsive w-48 h-48" src="${
                   post.thumbnail_url
@@ -154,7 +154,10 @@ const relativePost = async (category_id) => {
                               post._id
                             }')" id=modal-btn> 
                             
-                            <a href="#my-modal-2" class="btn btn-info"> More</a>
+                            <a href="#my-modal-2" class=" bg-sky-200"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6  mx-auto">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                          </svg>
+                          </a>
                             </button>
                             
                                                                      
